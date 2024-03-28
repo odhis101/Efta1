c5c2362f9c2823480a45624ecacfdc73f955b95a
odhis101 <joshodhiambo5@gmail.com>
2024-03-28 10:34:50 +0300

    Initial Commit

---
+++ EFTA1/EFTA1App.swift
@@ -0,0 +1,17 @@
+//
+//  EFTA1App.swift
+//  EFTA1
+//
+//  Created by Joshua on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct EFTA1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EFTA1UITests/EFTA1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EFTA1UITestsLaunchTests.swift
+//  EFTA1UITests
+//
+//  Created by Joshua on 3/28/24.
+//
+
+import XCTest
+
+class EFTA1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
